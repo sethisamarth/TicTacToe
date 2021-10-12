83bb7378971f9e9287b5f78fb586ef12977659d7
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-12 13:10:26 +0530

    checking user choice is valid or not

--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -15,17 +15,44 @@ namespace TicTacToeGame
 
         static void Main(string[] args)
         {
-           
 
-                Console.WriteLine("Player1:X and Player2:O");
 
-                Console.WriteLine("\n");
+            Console.WriteLine("Player1:X and Player2:O");
 
-                if (player % 2 == 0)//checking the chance of the player  
+            Console.WriteLine("\n");
+
+            if (player % 2 == 0)//checking the chance of the player  
+
+            {
+
+                Console.WriteLine("Player 2 Chance");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Player 1 Chance");
+
+            }
+
+            Console.WriteLine("\n");
+            Board();
+            choice = int.Parse(Console.ReadLine());//Taking users choice
+
+            // checking position where user want to mark x or o
+            if (arr[choice] != 'X' && arr[choice] != 'O')
+
+            {
+
+                if (player % 2 == 0) //if chance is of player 2 then mark O else mark X  
 
                 {
 
-                    Console.WriteLine("Player 2 Chance");
+                    arr[choice] = 'O';
+
+                    player++;
 
                 }
 
@@ -33,15 +60,21 @@ namespace TicTacToeGame
 
                 {
 
-                    Console.WriteLine("Player 1 Chance");
+                    arr[choice] = 'X';
+
+                    player++;
 
                 }
+            }
+            else //If there is any possition where user want to run and that is already marked then show message and load board again  
+
+            {
+
+                Console.WriteLine("Sorry the row {0} is already marked with {1}", choice, arr[choice]);
 
                 Console.WriteLine("\n");
-                Board();
-            choice = int.Parse(Console.ReadLine());//Taking users choice
-
-        }
+            }
+            }
 
         // create board method
         private static void Board()
@@ -67,4 +100,5 @@ namespace TicTacToeGame
             Console.WriteLine("     |     |      ");
         }
     }
+    
 }
