f3099e8bbb27623ac0989dfa20e6efb0c7eeff00
sethisamarth <90337565+sethisamarth@users.noreply.github.com>
2021-10-12 11:33:44 +0530

    making a board

--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -4,9 +4,42 @@ namespace TicTacToeGame
 {
     class Program
     {
+        //making array and   where no use of zero 
+
+        static char[] arr = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+
+        static int player = 1; //By default player 1 is set  
+
+        static int choice; //This holds the choice at which position user want to mark   
+
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Board();
+        }
+
+        // create board method
+        private static void Board()
+
+        {
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+
+            Console.WriteLine("_____|_____|_____ ");
+
+            Console.WriteLine("     |     |      ");
+
+            Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+
+            Console.WriteLine("     |     |      ");
         }
     }
 }
